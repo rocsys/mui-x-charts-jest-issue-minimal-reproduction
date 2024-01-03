e670d348528290b7cf1f5af7e7ba5455d5a4cbde
Robert Bakker <rbakker@rocsys.com>
2024-01-03 19:49:44 +0100

    reproduction bar chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TinyBarChart from './TinyBarChart';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <TinyBarChart />
       </header>
     </div>
   );
